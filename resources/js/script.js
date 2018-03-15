b039f8353a8f86f720ac636979448db604309bfa
RobinJohannesson <robinjohannesson96@gmail.com>
2018-03-16 00:53:38 +0100

    fungerande utskrift

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,19 @@
 $.getJSON( "resources/json/books.json", function( data ) {
     console.log(data);
 
+    var coursebefore = "";
     for (var key in data) {
-      console.log(data[key]);
-      $("p").append("<br><b>" + data[key].course + "</b><br>");
-      $("p").append("<i>" + data[key].name + "</i><br>");
-      $("p").append("<a href='" + data[key].link + "'>" + data[key].link + "</a><br>");
+      // console.log(data[key]);
+
+      if (data[key].course == coursebefore) {
+        $("p").append("<i class='name'>" + data[key].name + "</i><br>");
+        $("p").append("<a class='link' href='" + data[key].link + "'>" + data[key].link + "</a><br>");
+      }
+      else {
+        $("p").append("<br><b class='course'>" + data[key].course + "</b><br>");
+        $("p").append("<i class='name'>" + data[key].name + "</i><br>");
+        $("p").append("<a class='link' href='" + data[key].link + "'>" + data[key].link + "</a><br>");
+      }
+      coursebefore = data[key].course;
     }
   });
