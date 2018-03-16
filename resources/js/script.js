f95c7cd89f2eff0a405d8effcd408d252d0eeaab
RobinJohannesson <robinjohannesson96@gmail.com>
2018-03-16 11:43:50 +0100

    toggle function, not working

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,19 +1,28 @@
-$.getJSON( "resources/json/books.json", function( data ) {
-    console.log(data);
+$.getJSON("resources/json/books.json", function(data) {
+  console.log(data);
 
-    var coursebefore = "";
-    for (var key in data) {
-      // console.log(data[key]);
+  var coursebefore = "";
+  for (var key in data) {
+    // console.log(data[key]);
 
-      if (data[key].course == coursebefore) {
-        $("p").append("<i class='name'>" + data[key].name + "</i><br>");
-        $("p").append("<a class='link' href='" + data[key].link + "'>" + data[key].link + "</a><br>");
-      }
-      else {
-        $("p").append("<br><b class='course'>" + data[key].course + "</b><br>");
-        $("p").append("<i class='name'>" + data[key].name + "</i><br>");
-        $("p").append("<a class='link' href='" + data[key].link + "'>" + data[key].link + "</a><br>");
-      }
-      coursebefore = data[key].course;
+    if (data[key].course == coursebefore) {
+      $("p").append("<i class='name'>" + data[key].name + "</i><br>");
+      $("p").append("<a class='link' href='" + data[key].link + "'>" + data[key].link + "</a><br>");
     }
+    else if (data[key].course == "") {
+      $("p").append("<b class='course'>" + data[key].course + "</b><br>");
+      $("p").append("<i class='name'>" + data[key].name + "</i><br>");
+      $("p").append("<a class='link' href='" + data[key].link + "'>" + data[key].link + "</a><br>");
+    }
+    else {
+      $("p").append("<b class='course'>" + data[key].course + "</b><br>");
+      $("p").append("<i class='name'>" + data[key].name + "</i><br>");
+      $("p").append("<a class='link' href='" + data[key].link + "'>" + data[key].link + "</a><br>");
+    }
+    coursebefore = data[key].course;
+  }
+  $(".course").click(function() {
+    $(".link").toggle();
+    console.log("click");
   });
+});
